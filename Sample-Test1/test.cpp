fc5d0963d890e028445505eadb58b406e4888205
kwanghun <lighthun@gmail.com>
2023-06-02 11:22:25 +0900

    first commit

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "../Project_SimiliarityChecker/similarityChecker.cpp"
+
+TEST(TestCaseName, TestName) {
+	SimiliarityChecker sm_checker;
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
