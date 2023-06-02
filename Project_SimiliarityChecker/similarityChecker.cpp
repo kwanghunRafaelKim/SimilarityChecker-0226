b135d0f96d9d78ae19c30738877093ef3ee4ae6f
kwanghun <lighthun@gmail.com>
2023-06-02 11:51:47 +0900

    implement GAP funtion

--- Project_SimiliarityChecker/similarityChecker.cpp
+++ Project_SimiliarityChecker/similarityChecker.cpp
@@ -5,8 +5,25 @@ using namespace std;
 class SimiliarityChecker
 {
 public:
+	SimiliarityChecker(const string& minputstr1, const string& minputstr2)
+		: minputstr1(minputstr1),
+		  minputstr2(minputstr2)
+	{
+	}
+
 	int getStrSize(string inputStr)
 	{
 		return inputStr.size();
 	}
+
+	int getGAPfromStrSize(void)
+	{
+		int GAP = getStrSize(minputstr1) - getStrSize(minputstr2) > 0 ?
+			      getStrSize(minputstr1) - getStrSize(minputstr2) :
+		          getStrSize(minputstr2) - getStrSize(minputstr1);
+		return GAP;
+	}
+
+private:
+	string minputstr1, minputstr2;
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,7 +2,20 @@
 #include "../Project_SimiliarityChecker/similarityChecker.cpp"
 
 TEST(SimilarityChecker, CheckStrSize) {
-  SimiliarityChecker sm_checker{};
+  SimiliarityChecker sm_checker{ "ABC" ,"CDEF"};
   string inputStr = "ABC";
   EXPECT_EQ(3, sm_checker.getStrSize(inputStr));  
+}
+
+TEST(SimilarityChecker, getGAPfromStrSizeDiff1) {
+	SimiliarityChecker sm_checker{ "ABC" ,"CDEF" };
+	EXPECT_EQ(1, sm_checker.getGAPfromStrSize());
+}
+TEST(SimilarityChecker, getGAPfromStrSizeDiff2) {
+	SimiliarityChecker sm_checker{ "ABCDEF" ,"CDEF" };
+	EXPECT_EQ(2, sm_checker.getGAPfromStrSize());
+}
+TEST(SimilarityChecker, getGAPfromStrSizeDiff0) {
+	SimiliarityChecker sm_checker{ "ABCD" ,"CDEF" };
+	EXPECT_EQ(0, sm_checker.getGAPfromStrSize());
 }
